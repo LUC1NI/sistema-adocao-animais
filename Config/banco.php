975acbd61af13a0f1de73f9c6bddc912c75402b2
JOAO VITOR LUCINI <joao.lucini@cs.up.edu.br>
2025-06-03 20:34:21 -0300

    Criacao do projeto base e do banco de dados

---
+++ Config/banco.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "adocao";
+
+$banco = new mysqli($host, $user, $pass, $dbname);
+if ($banco->connect_errno) {
+    die("Erro de conexão: " . $banco->connect_error);
+}
+?>
